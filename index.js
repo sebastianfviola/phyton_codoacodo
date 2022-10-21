f6a86ba6f68d267605741b434b793103e1ebcedc
sebastianfviola <sviola@tecnolab.com.ar>
2022-10-21 08:12:15 -0300

    scroll validación

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function valida_envia(){
     //valido el nombre
     if (document.fvalida.nombre.value.length==0){
            alert("Tiene que escribir su nombre")
-           document.fvalida.nombre.focus()
+           //document.fvalida.nombre.focus()
+           document.getElementById("nombre").focus();
            return 0;
     }
 
