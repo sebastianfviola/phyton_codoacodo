7814182f9e4b4d64b9149b256424420a6b490a02
sebastianfviola <sviola@tecnolab.com.ar>
2022-10-20 22:51:56 -0300

    Submit Formulario

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //archivo javascript
+
+function valida_envia(){
+    //valido el nombre
+    if (document.fvalida.nombre.value.length==0){
+           alert("Tiene que escribir su nombre")
+           document.fvalida.nombre.focus()
+           return 0;
+    }
+
+       	//el formulario se envia
+           alert("Muchas gracias por enviar el formulario");
+           document.fvalida.submit();
+
+}
+
