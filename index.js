327cdcbc0e13c02804d9bac0bf6aefcf7a801e3e
sebastianfviola <sviola@tecnolab.com.ar>
2022-11-23 21:53:36 -0300

    ultima modificacion

--- index.js
+++ index.js
@@ -2,15 +2,27 @@
 
 function valida_envia(){
     //valido el nombre
-    if (document.fvalida.nombre.value.length==0){
-           alert("Tiene que escribir su nombre")
-           //document.fvalida.nombre.focus()
-           document.getElementById("nombre").focus();
-           return 0;
-    }
+    let x = document.forms["fvalida"]["nombre"].value;
+if (x == "") {
+  alert("Debe ingresar su nombre");
+  return false;
+}
+let y = document.forms["fvalida"]["email"].value;
+if (y == "") {
+  alert("Debe ingresar su email");
+  return false;
+}
 
+let z = document.forms["fvalida"]["message"].value;
+if (z == "") {
+  alert("Debe ingresar un mensaje");
+  return false;
+}
+        
        	//el formulario se envia
            alert("Muchas gracias por enviar el formulario");
            document.fvalida.submit();
 
 }
+
+
